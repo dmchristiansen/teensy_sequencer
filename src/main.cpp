32958ff0db6e74b3aed74f0ead06cb55fdec157c
Daniel Christiansen <christiansen.daniel@gmail.com>
2019-02-25 14:39:55 -0800

    messing around

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,64 @@
 #include "WProgram.h"
+#include "controller.h"
+#include "ui.h"
+#include "system.h"
+
+SequenceController controller;
+UI ui;
+
 
 extern "C" int main(void)
 {
-#ifdef USING_MAKEFILE
-
-	// To use Teensy 3.0 without Arduino, simply put your code here.
-	// For example:
-
 	pinMode(13, OUTPUT);
-	while (1) {
-		digitalWriteFast(13, HIGH);
-		delay(500);
-		digitalWriteFast(13, LOW);
-		delay(500);
-	}
+	digitalWrite(13, LOW);
+
+	ui.init();
+	controller.init(&ui);
+
+	while (1) {}
 
 
-#else
-	// Arduino's main() function just calls setup() and loop()....
-	setup();
-	while (1) {
-		loop();
-		yield();
-	}
-#endif
 }
 
+void systick_isr(void) {
+
+	// poll system
+	ui.poll();
+
+}
+
+void porta_isr(void) {
+	
+	// check if the interrupt was on pa1
+	if (PORTA_ISFR & 0x2) {
+		PORTA_ISFR = 0x00000002;		// clear interrupt
+		controller.advance();	// respond to clock edge
+	}
+
+}
+
+
+/*
+ * Program flow:
+ *
+ * systick (1kHz) triggers controller to assess state
+ * - on clk interrupt pending, 
+ *   - advance step
+ *   - update nominal step period
+ *   - clean read from ui
+ *   - update state
+ * - on no interrupt, 
+ *   - update read from ui
+ *   - update state
+ *
+ * ui state update:
+ * 	- read from cv_input
+ * 	 - read waiting adc conversion
+ * 	 - start next conversion
+ * 	 - map & lpf cv inputs
+ * 	- read from switch_input
+ * 	 - scan through switch inputs, interpret result
+ *
+ *
+ *
+ */
